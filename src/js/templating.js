584fe74715a622dd95822f0c71460cb30274fbd8
Serhii Koziuba <preborist@gmail.com>
2020-12-05 18:45:33 +0200

    second commit

---
+++ src/js/templating.js
@@ -0,0 +1,7 @@
+import menuItems from '../menu.json';
+import itemsTemplate from './templates/menu-items.hbs';
+
+const menuRef = document.querySelector('.menu.js-menu');
+
+const markup = itemsTemplate(menuItems);
+menuRef.insertAdjacentHTML('beforeend', markup);
